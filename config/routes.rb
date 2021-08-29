6260fd2c7c8e619f3b09f0373cd044a32e46e5fd
mai174143 <ikura58n@gmail.com>
2021-08-29 06:51:18 +0000

    user controller and model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get  'signup' => "users#new"
   root 'static_pages#home'
   get 'home' => "static_pages#home"
   get 'help' => "static_pages#help"
@@ -7,6 +8,5 @@ Rails.application.routes.draw do
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'hello-world' => "foo#hello"
 
 end
