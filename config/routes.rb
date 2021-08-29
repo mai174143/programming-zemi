24f19fa2e04f9dbe15e9b23b0d13a0d23bf3b76a
mai174143 <ikura58n@gmail.com>
2021-08-29 11:26:24 +0000

    basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'contact' => "static_pages#contact"
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get 'signup' => 'users#new'
+  resources :users
 
 end
